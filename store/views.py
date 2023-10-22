7192759ea40b8c6fc8d3b921cdf4ba272db2de65
Sam Ferguson <sam@thirstyit.com.au>
2023-10-22 16:07:11 +1100

    Updated views.py

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def store(request, category_slug=None):
     categories = None
@@ -14,10 +15,13 @@ def store(request, category_slug=None):
         product_count= products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products,6) 
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
