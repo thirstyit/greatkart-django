583dc44d02b52f2cfe705793c0bc10805125ed6f
Sam Ferguson <sam@thirstyit.com.au>
2023-10-31 20:58:36 +1100

    addition of pagination and search

--- store/views.py
+++ store/views.py
@@ -4,6 +4,8 @@ from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
+from django.db.models import Q
 
 def store(request, category_slug=None):
     categories = None
@@ -12,9 +14,12 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products,6) 
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count= products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         paginator = Paginator(products,6) 
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
@@ -39,3 +44,14 @@ def product_detail(request, category_slug, product_slug):
     }
 
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+   
+    context = {
+        'products' : products,
+    }
+    return render(request, 'store/store.html', context)
